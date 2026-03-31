bd7058fb938ad7981c6e5e2ded7831e630b82021
Bohdan Baran <b.baran@24hit.games>
2026-03-31 17:13:50 +0300

    Added new logic for Enemy AI behaviour

--- Assets/FPS/Scripts/AI/EnemyMobile.cs
+++ Assets/FPS/Scripts/AI/EnemyMobile.cs
@@ -11,6 +11,7 @@ namespace Unity.FPS.AI
             Patrol,
             Follow,
             Attack,
+            Search,
         }
 
         public Animator Animator;
@@ -25,12 +26,36 @@ namespace Unity.FPS.AI
         public ParticleSystem[] OnDetectVfx;
         public AudioClip OnDetectSfx;
 
+        [Header("Search")]
+        [Tooltip("Minimum time spent searching at the last known target position before returning to patrol")]
+        public float SearchDurationMin = 3f;
+
+        [Tooltip("Maximum time spent searching at the last known target position before returning to patrol")]
+        public float SearchDurationMax = 5f;
+
+        [Tooltip("Rotation speed while searching the last known target position")]
+        public float SearchRotationSpeed = 60f;
+
+        [Header("Debug")]
+        [Tooltip("Draw the last known target position while selected")]
+        public Color SearchPositionGizmoColor = Color.cyan;
+
+        [Tooltip("Enable logs when the mobile enemy changes AI state")]
+        public bool LogStateChanges;
+
         [Header("Sound")] public AudioClip MovementSound;
         public MinMaxFloat PitchDistortionMovementSpeed;
 
         public AIState AiState { get; private set; }
         EnemyController m_EnemyController;
         AudioSource m_AudioSource;
+        bool m_WasSeeingTarget;
+        bool m_IsAlerted;
+        bool m_HasLastKnownTargetPosition;
+        bool m_SearchReachedLastKnownPosition;
+        Vector3 m_LastKnownTargetPosition;
+        float m_SearchDuration;
+        float m_SearchStartTime = Mathf.NegativeInfinity;
 
         const string k_AnimMoveSpeedParameter = "MoveSpeed";
         const string k_AnimAttackParameter = "Attack";
@@ -76,23 +101,64 @@ namespace Unity.FPS.AI
 
         void UpdateAiStateTransitions()
         {
+            bool hasKnownTarget = m_EnemyController.KnownDetectedTarget != null;
+            bool isSeeingTarget = m_EnemyController.IsSeeingTarget;
+
+            if (hasKnownTarget && isSeeingTarget)
+            {
+                RememberTargetPosition(m_EnemyController.KnownDetectedTarget.transform.position);
+                SetAlertVisuals(true, false);
+                SetAiState(m_EnemyController.IsTargetInAttackRange ? AIState.Attack : AIState.Follow);
+                m_WasSeeingTarget = true;
+                return;
+            }
+
+            if (m_WasSeeingTarget && !isSeeingTarget && hasKnownTarget)
+            {
+                StartSearch();
+            }
+
+            m_WasSeeingTarget = isSeeingTarget;
+
             // Handle transitions 
             switch (AiState)
             {
                 case AIState.Follow:
-                    // Transition to attack when there is a line of sight to the target
-                    if (m_EnemyController.IsSeeingTarget && m_EnemyController.IsTargetInAttackRange)
+                    if (!hasKnownTarget)
                     {
-                        AiState = AIState.Attack;
-                        m_EnemyController.SetNavDestination(transform.position);
+                        if (m_HasLastKnownTargetPosition)
+                        {
+                            StartSearch();
+                        }
+                        else
+                        {
+                            ResumePatrol();
+                        }
                     }
 
                     break;
                 case AIState.Attack:
-                    // Transition to follow when no longer a target in attack range
-                    if (!m_EnemyController.IsTargetInAttackRange)
+                    if (!hasKnownTarget)
                     {
-                        AiState = AIState.Follow;
+                        if (m_HasLastKnownTargetPosition)
+                        {
+                            StartSearch();
+                        }
+                        else
+                        {
+                            ResumePatrol();
+                        }
+                    }
+
+                    break;
+                case AIState.Search:
+                    if (!m_HasLastKnownTargetPosition)
+                    {
+                        ResumePatrol();
+                    }
+                    else if (m_SearchReachedLastKnownPosition && Time.time >= m_SearchStartTime + m_SearchDuration)
+                    {
+                        ResumePatrol();
                     }
 
                     break;
@@ -109,11 +175,21 @@ namespace Unity.FPS.AI
                     m_EnemyController.SetNavDestination(m_EnemyController.GetDestinationOnPath());
                     break;
                 case AIState.Follow:
+                    if (m_EnemyController.KnownDetectedTarget == null)
+                    {
+                        break;
+                    }
+
                     m_EnemyController.SetNavDestination(m_EnemyController.KnownDetectedTarget.transform.position);
                     m_EnemyController.OrientTowards(m_EnemyController.KnownDetectedTarget.transform.position);
                     m_EnemyController.OrientWeaponsTowards(m_EnemyController.KnownDetectedTarget.transform.position);
                     break;
                 case AIState.Attack:
+                    if (m_EnemyController.KnownDetectedTarget == null)
+                    {
+                        break;
+                    }
+
                     if (Vector3.Distance(m_EnemyController.KnownDetectedTarget.transform.position,
                             m_EnemyController.DetectionModule.DetectionSourcePoint.position)
                         >= (AttackStopDistanceRatio * m_EnemyController.DetectionModule.AttackRange))
@@ -128,6 +204,27 @@ namespace Unity.FPS.AI
                     m_EnemyController.OrientTowards(m_EnemyController.KnownDetectedTarget.transform.position);
                     m_EnemyController.TryAtack(m_EnemyController.KnownDetectedTarget.transform.position);
                     break;
+                case AIState.Search:
+                    m_EnemyController.SetNavDestination(m_LastKnownTargetPosition);
+
+                    if ((transform.position - m_LastKnownTargetPosition).magnitude <= m_EnemyController.PathReachingRadius)
+                    {
+                        if (!m_SearchReachedLastKnownPosition)
+                        {
+                            m_SearchReachedLastKnownPosition = true;
+                            m_SearchStartTime = Time.time;
+                        }
+
+                        m_EnemyController.SetNavDestination(transform.position);
+                        transform.Rotate(Vector3.up, SearchRotationSpeed * Time.deltaTime, Space.Self);
+                    }
+                    else
+                    {
+                        m_SearchReachedLastKnownPosition = false;
+                    }
+
+                    m_EnemyController.OrientWeaponsTowards(m_LastKnownTargetPosition);
+                    break;
             }
         }
 
@@ -138,37 +235,25 @@ namespace Unity.FPS.AI
 
         void OnDetectedTarget()
         {
-            if (AiState == AIState.Patrol)
+            if (m_EnemyController.KnownDetectedTarget != null)
             {
-                AiState = AIState.Follow;
+                RememberTargetPosition(m_EnemyController.KnownDetectedTarget.transform.position);
             }
 
-            for (int i = 0; i < OnDetectVfx.Length; i++)
-            {
-                OnDetectVfx[i].Play();
-            }
-
-            if (OnDetectSfx)
-            {
-                AudioUtility.CreateSFX(OnDetectSfx, transform.position, AudioUtility.AudioGroups.EnemyDetection, 1f);
-            }
-
-            Animator.SetBool(k_AnimAlertedParameter, true);
+            SetAiState(m_EnemyController.IsTargetInAttackRange ? AIState.Attack : AIState.Follow);
+            SetAlertVisuals(true, true);
         }
 
         void OnLostTarget()
         {
-            if (AiState == AIState.Follow || AiState == AIState.Attack)
+            if ((AiState == AIState.Follow || AiState == AIState.Attack || AiState == AIState.Search) &&
+                m_HasLastKnownTargetPosition)
             {
-                AiState = AIState.Patrol;
+                StartSearch();
+                return;
             }
 
-            for (int i = 0; i < OnDetectVfx.Length; i++)
-            {
-                OnDetectVfx[i].Stop();
-            }
-
-            Animator.SetBool(k_AnimAlertedParameter, false);
+            ResumePatrol();
         }
 
         void OnDamaged()
@@ -180,6 +265,105 @@ namespace Unity.FPS.AI
             }
 
             Animator.SetTrigger(k_AnimOnDamagedParameter);
+
+            if (m_EnemyController.KnownDetectedTarget != null)
+            {
+                RememberTargetPosition(m_EnemyController.KnownDetectedTarget.transform.position);
+                SetAlertVisuals(true, false);
+                SetAiState(IsTargetWithinAttackRange(m_EnemyController.KnownDetectedTarget.transform.position)
+                    ? AIState.Attack
+                    : AIState.Follow);
+            }
+        }
+
+        void StartSearch()
+        {
+            if (!m_HasLastKnownTargetPosition)
+            {
+                ResumePatrol();
+                return;
+            }
+
+            m_SearchDuration = Random.Range(Mathf.Min(SearchDurationMin, SearchDurationMax),
+                Mathf.Max(SearchDurationMin, SearchDurationMax));
+            m_SearchReachedLastKnownPosition = false;
+            m_SearchStartTime = Mathf.NegativeInfinity;
+            SetAiState(AIState.Search);
+        }
+
+        void ResumePatrol()
+        {
+            SetAiState(AIState.Patrol);
+            m_SearchReachedLastKnownPosition = false;
+            m_SearchStartTime = Mathf.NegativeInfinity;
+            m_EnemyController.SetPathDestinationToClosestNode();
+            SetAlertVisuals(false, false);
+        }
+
+        void RememberTargetPosition(Vector3 targetPosition)
+        {
+            m_LastKnownTargetPosition = targetPosition;
+            m_HasLastKnownTargetPosition = true;
+        }
+
+        bool IsTargetWithinAttackRange(Vector3 targetPosition)
+        {
+            return Vector3.Distance(targetPosition, m_EnemyController.DetectionModule.DetectionSourcePoint.position) <=
+                   m_EnemyController.DetectionModule.AttackRange;
+        }
+
+        void SetAiState(AIState newState)
+        {
+            if (AiState == newState)
+            {
+                return;
+            }
+
+            AiState = newState;
+
+            if (LogStateChanges)
+            {
+                Debug.Log($"{name} AI state -> {AiState}", this);
+            }
+        }
+
+        void SetAlertVisuals(bool alerted, bool playDetectFeedback)
+        {
+            if (!m_IsAlerted && alerted && playDetectFeedback)
+            {
+                for (int i = 0; i < OnDetectVfx.Length; i++)
+                {
+                    OnDetectVfx[i].Play();
+                }
+
+                if (OnDetectSfx)
+                {
+                    AudioUtility.CreateSFX(OnDetectSfx, transform.position, AudioUtility.AudioGroups.EnemyDetection,
+                        1f);
+                }
+            }
+            else if (m_IsAlerted && !alerted)
+            {
+                for (int i = 0; i < OnDetectVfx.Length; i++)
+                {
+                    OnDetectVfx[i].Stop();
+                }
+            }
+
+            m_IsAlerted = alerted;
+            Animator.SetBool(k_AnimAlertedParameter, alerted);
+        }
+
+        void OnDrawGizmosSelected()
+        {
+            if (!m_HasLastKnownTargetPosition)
+            {
+                return;
+            }
+
+            Gizmos.color = SearchPositionGizmoColor;
+            Gizmos.DrawWireSphere(m_LastKnownTargetPosition, 0.5f);
+            Gizmos.DrawLine(transform.position, m_LastKnownTargetPosition);
         }
     }
 }
